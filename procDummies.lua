261012ab05aa24d1eba66fd90e907306c42ec253
Proclarush <devproclarush@protonmail.com>
2023-11-11 14:56:55 +0000

    Initial Commit

---
+++ procDummies.lua
@@ -0,0 +1,159 @@
+function file_exists(path)
+    local f = io.open(path, "r")
+    if f then f:close()end
+    return f ~= nil
+end
+
+function require_if_exists(file)
+    local user_profile = os.getenv("USERPROFILE")
+    local directory = user_profile .. "\\Documents\\MemoryError\\Lua_Scripts\\procDummies\\"
+    local filename = file:gsub("%.", "\\")
+    local path = directory .. filename .. ".lua"
+    
+print(file_exists(path))
+
+    if file_exists(path) then
+        local path_to_add = directory .. "?.lua"
+        if not string.find(package.path, path_to_add, 1, true) then
+            package.path = package.path .. ";" .. path_to_add
+        end
+        return require(file)
+    end
+end
+
+local status, module_or_error = pcall(require_if_exists, "procGUI")
+local procGUI = {}
+local API = require("api")
+
+    os.execute("cls")
+    print("#####################################")
+    print("#     Starting Script ProcDummies   #")
+    print("#####################################")
+if (status) then
+    print("#  procGUI.lua Loaded Successfully  #")
+    print("#####################################")
+    procGUI = module_or_error
+    procGUI.Init()
+    API.Write_ScripCuRunning0("procDummies")
+else
+    print("#        procGUI.lua missing        #")
+    print("#      No Functionality Loaded      #")
+    print("#####################################")
+    print(module_or_error)
+end
+
+local scriptFirstRun = true
+
+local function FirstRun()
+    local player = API.GetLocalPlayerName()
+    API.Write_ScripCuRunning0("procDummies: " .. player)
+    scriptFirstRun = false;
+end
+
+local startTime2 = os.time()
+local idleTimeThreshold = math.random(120, 260)
+
+local function AntiAFK()
+    local currentTime = os.time()
+    local elapsedTime = os.difftime(currentTime, startTime2)
+
+    if elapsedTime >= idleTimeThreshold then
+        API.PIdle2()
+        startTime2 = os.time()
+        idleTimeThreshold = math.random(200, 280)
+        print("Reset Timer & Threshhold")
+    end
+end
+
+local function PerformAction(itemId) 
+    local i = 0
+    local inv = API.ReadInvArrays33()
+    if #inv > 0 then
+        for _, a in pairs(inv) do
+            if(a.itemid1 == itemId) then
+                API.DoAction_Interface(0x24,0x8d63,2,1473,5,i,5392);
+                API.RandomSleep2(200, 400, 800)
+            end
+            i = i + 1
+        end
+    end
+end
+
+local function ActionDummy()
+    local locationToChosenValue = {
+        Slayer = 36195, Agility = 37700, Theiving = 36194, Archaeology = 54021, Dungeoneering = 54020, Combat = 50442
+    }
+
+    print(dummiesList.string_value)
+    local chosenValue = locationToChosenValue[dummiesList.string_value]
+    print(locationToChosenValue[dummiesList.string_value])
+
+    if chosenValue then
+        print("Deploying " .. chosenValue)
+        PerformAction(locationToChosenValue[dummiesList.string_value])
+        API.RandomSleep2(200, 250, 350)
+    end
+end
+
+local function AnimCheck(loops)
+    local player = API.GetLocalPlayerName()
+    if(API.IsPlayerAnimating_(player, loops)) then
+        print("Animating, not placing")
+        API.RandomSleep2(2000, 2500, 3500)
+        return true
+    end
+    print("Not animating, placing new dummy")
+    return false
+end
+
+
+local function procDummies()
+
+    local player = API.GetLocalPlayerName()
+    local isWorking = API.IsPlayerAnimating_(player, 5)
+
+    if(not AnimCheck(150)) then
+        if(not AnimCheck(15)) then
+            ActionDummy()
+            API.RandomSleep2(200, 250, 350)
+        end
+    end
+end
+
+API.Write_LoopyLoop(true)
+while(API.Read_LoopyLoop())
+do-----------------------------------------------------------------------------------
+
+    procGUI.Draw()
+
+    if (API.GetGameState() == 2) then
+        API.KeyPress_(" ")
+    end
+
+    if(API.GetGameState() == 3) then
+        
+
+        AntiAFK()
+        API.DoRandomEvents()
+
+        if(dummiesList.string_value == "Select Action" or dummiesList.string_value == nil or dummiesList.string_value == "" or dummiesList.string_value == " ") then
+            print("Waiting for user to finish setup")
+        else
+
+            if(scriptFirstRun) then 
+                print("First run!")
+                FirstRun() 
+                if(string.upper(dummiesList.string_value) ~= "Combat") then
+                    procGUI.Config(string.upper(dummiesList.string_value))
+                end
+            end
+
+            procDummies()
+        end
+    end
+
+
+
+
+    API.RandomSleep2(500, 750, 1500)
+end----------------------------------------------------------------------------------
---
+++ procDummies/procGUI.lua
@@ -0,0 +1,135 @@
+local procDummiesGUI = {}
+local API = require("api")
+
+local scriptStartingTime = os.time()
+local skillStartingXP = 0
+local setup = false
+
+function procDummiesGUI.Config(skill) 
+
+    skillStartingXP = API.GetSkillXP(string.upper(dummiesList.string_value))
+
+    xpGained = API.CreateIG_answer()
+    xpGained.box_start = FFPOINT.new(80, 130, 0)
+    xpGained.colour = ImColor.new(141, 145, 1)
+    xpGained.box_name = "xpGained"
+
+    xpGained.string_value = "Gained " .. procDummiesGUI.CommaFormatting((API.GetSkillXP(string.upper(dummiesList.string_value)) -skillStartingXP)) .. " XP"
+    API.DrawTextAt(xpGained)
+
+    timeTillLevel = API.CreateIG_answer()
+    timeTillLevel.box_start = FFPOINT.new(93, 165, 0)
+    timeTillLevel.colour = ImColor.new(141, 145, 1)
+    timeTillLevel.box_name = "ttl"
+    timeTillLevel.string_value = "TTL 00:00:00"
+
+    timeTillLevel.string_value = "TTL " .. (procDummiesGUI.CalculateTimeFrame() or "00:00:00")
+    API.DrawTextAt(timeTillLevel)
+
+    setup = true
+end
+
+function procDummiesGUI.Init()
+    backgroundPanel = API.CreateIG_answer();
+    backgroundPanel.box_start = FFPOINT.new(10, 100, 0)
+    backgroundPanel.box_size = FFPOINT.new(250, 235, 0)
+    backgroundPanel.colour = ImColor.new(15, 13, 18, 150)
+
+    titleBarText = API.CreateIG_answer()
+    titleBarText.box_start = FFPOINT.new(48, 100, 0)
+    titleBarText.colour = ImColor.new(141, 145, 1)
+    titleBarText.box_name = "titleBar"
+    titleBarText.string_value = "### procDummies v1.0 ###"
+
+    dummiesList = API.CreateIG_answer()
+    dummiesList.box_name = "     "
+    dummiesList.box_start = FFPOINT.new(79, 205, 0)
+    dummiesList.stringsArr = {"Select Action", "Slayer", "Agility", "Thieving", "Archaeology", "Dungeoneering", "Combat"}
+
+
+    totalTimeRunning = API.CreateIG_answer()
+    totalTimeRunning.box_start = FFPOINT.new(105, 196, 0)
+    totalTimeRunning.colour = ImColor.new(141, 145, 1)
+    totalTimeRunning.box_name = "ttr"
+    totalTimeRunning.string_value = procDummiesGUI.Runtime()
+end
+
+function procDummiesGUI.Draw()
+    API.DrawSquareFilled(backgroundPanel)
+    API.DrawTextAt(titleBarText)
+    API.DrawComboBox(dummiesList, false)
+
+    if(setup) then
+        print("Setup, drawing XP: " .. tostring((API.GetSkillXP(string.upper(dummiesList.string_value)) -skillStartingXP)))
+        xpGained.string_value = "Gained " .. procDummiesGUI.CommaFormatting((API.GetSkillXP(string.upper(dummiesList.string_value)) -skillStartingXP)) .. " XP"
+        API.DrawTextAt(xpGained)
+
+        print("Setup, drawing TTL: " .. tostring((procDummiesGUI.CalculateTimeFrame() or "00:00:00")))
+        timeTillLevel.string_value = "TTL " .. (procDummiesGUI.CalculateTimeFrame() or "00:00:00")
+        API.DrawTextAt(timeTillLevel)
+    end
+
+    totalTimeRunning.string_value = procDummiesGUI.Runtime()
+    API.DrawTextAt(totalTimeRunning)
+end
+
+function procDummiesGUI.Runtime()
+    local diff = os.difftime(os.time(), scriptStartingTime)
+    local hours = math.floor(diff / 3600)
+    local minutes = math.floor((diff % 3600) / 60)
+    local seconds = diff % 60
+    return string.format("%02d:%02d:%02d", hours, minutes, seconds)
+end
+
+function procDummiesGUI.RoundNumber(value, decimalPlaces)
+    local multiplier = 10^(decimalPlaces or 0)
+    return math.floor(value * multiplier + 0.5) / multiplier
+end
+
+function procDummiesGUI.CommaFormatting(number)
+    local formatted = tostring(number)
+    local k
+    while true do  
+        formatted, k = formatted:gsub("^(-?%d+)(%d%d%d)", '%1,%2')
+        if (k==0) then
+            break
+        end
+    end
+    return formatted
+end
+
+function procDummiesGUI.CalculateTimeFrame()
+
+    local currentXp = API.GetSkillXP(string.upper(dummiesList.string_value))
+    local elapsedMinutes = (os.time() - scriptStartingTime) / 60
+    local diffXp = math.abs(currentXp - skillStartingXP);
+    local xpPH = procDummiesGUI.RoundNumber((diffXp * 60) / elapsedMinutes);
+
+    local currentXp = API.GetSkillXP(string.upper(dummiesList.string_value))
+    local xpNeededForCondition = API.XPForLevel(API.XPLevelTable(currentXp) + 1)
+
+    local xpRemaining = xpNeededForCondition - currentXp
+
+    if xpPH == 0 then
+        return
+    end
+
+    local hoursNeeded = xpRemaining / xpPH
+
+    local wholeHours = math.floor(hoursNeeded)
+    local minutesNeeded = math.floor((hoursNeeded - wholeHours) * 60)
+    local secondsNeeded = math.floor((((hoursNeeded - wholeHours) * 60) - minutesNeeded) * 60)
+
+    local success, timeNeededStr = pcall(string.format, "%02d:%02d:%02d", wholeHours, minutesNeeded, secondsNeeded)
+    if not success then
+        return
+    end
+
+    return timeNeededStr
+end
+
+function procDummiesGUI.CountItems(itemID)
+    return API.InvStackSize(itemID)
+end
+
+return procDummiesGUI
